0f08e556dcb9efe7fc40623a8c48acee0d3a596d
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-15 18:02:17 +0300

    @hapi/joi projeye dahil edildi, Joi objesi ile validasyon işlemi gerçekleştirildi, validasyon sağlanamazsa 400 ve error message dönüldü

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,27 @@
 const router = require('express').Router()
+//model
 const User = require('../models/User')
+//validation
+const Joi = require('@hapi/joi')
+
+//validation schema
+const validateSchema = Joi.object({
+    isim:Joi.string().min(6).required(),
+    email:Joi.string().min(6).required().email(),
+    parola:Joi.string().min(6).required()
+})
 
 router.post('/register', async (req,res) => {
     //res.send('Register işlemi gerçekleşti')
+
+    //kullanıcı - valid şema uyum kontrolü
+    //res.send(validateSchema.validate(req.body))
+
+    const {error} = validateSchema.validate(req.body)
+
+    //validasyon sağlanamazsa 400 dön , hata detay mesajını gönder
+    if(error) return res.status(400).send(error.details[0].message)
+
     const user = new User({
         isim:req.body.isim,
         email:req.body.email,
